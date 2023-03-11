146a0dfa7d69d398501694de7c3a77263d911a32
carvtmux-sessionizeralho28 <carvalhoo287@gmail.com>
2023-03-11 14:49:56 +0000

    First commit

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <div>
+      {/* overlay */}
+      <div></div>
+    </div>
+  )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import "@/styles/index.css"
+
+import type { AppProps } from "next/app"
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
