30d28efec65a07b7293bdf0e1da97c1b9b074937
carvtmux-sessionizeralho28 <carvalhoo287@gmail.com>
2023-03-12 20:06:33 +0000

    Added confirm email and solved some visual bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
-import "@/styles/globals.css"
-import "@/styles/index.css"
-import "@/styles/login.css"
+import "@/styles/globals.css";
+import "@/styles/index.css";
+import "@/styles/login.css";
 
-import type { AppProps } from "next/app"
-import { Poppins } from "next/font/google"
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
-})
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={poppins.className}>
       <Component {...pageProps} />
     </main>
-  )
+  );
 }
--- pages/register.tsx
+++ pages/register.tsx
@@ -6,17 +6,20 @@ import { FaGithub, FaTwitter } from "react-icons/fa";
 import { useState } from "react";
 import supabase from "@/utils/supabaseClient";
 import { useRouter } from "next/router";
+import ConfirmEmail from "@/components/ConfirmEmail";
 
 export default function Register() {
-  const router = useRouter();
-
   const [name, setName] = useState<string | undefined>();
   const [email, setEmail] = useState<string | undefined>();
   const [password, setPassword] = useState<string | undefined>();
   const [confirmPassword, setConfirmPassword] = useState<string | undefined>();
 
+  const [regSuccess, setRegSuccess] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
   async function signUpEmail(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    setLoading(true);
     try {
       if (name && email && password && confirmPassword) {
         console.log(email, password, confirmPassword);
@@ -34,6 +37,10 @@ export default function Register() {
           throw resp.error.message;
         }
         const userId = resp.data.user?.id;
+        if (userId) {
+          setLoading(false);
+          setRegSuccess(true);
+        }
       }
     } catch (error) {
       throw error;
@@ -41,11 +48,12 @@ export default function Register() {
   }
 
   return (
-    <div className="h-screen login-background">
+    <div className="min-h-screen login-background">
       <Header color={0} />
-      <div className="relative isolate pt-2">
-        <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-24 lg:px-8 lg:py-40">
-          <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24 bg-white2 rounded-3xl h-fit shadow-lg m-6">
+      {regSuccess && <ConfirmEmail />}
+      <div className="relative isolate">
+        <div className="mx-auto max-w-7xl px-6 lg:flex lg:items-center lg:gap-x-24 lg:px-8">
+          <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24 bg-white2 rounded-3xl shadow-lg">
             <div className="mx-auto w-full max-w-sm lg:w-96">
               <div>
                 <h2 className="mt-6 text-3xl font-bold tracking-tight text-black text-center">
@@ -191,6 +199,30 @@ export default function Register() {
                           onChange={(e) => setConfirmPassword(e.target.value)}
                         />
                       </div>
+
+                      {loading && (
+                        <div className="flex justify-center">
+                          <div role="status">
+                            <svg
+                              aria-hidden="true"
+                              className="inline w-10 h-10 mr-2 mt-5 text-gray-200 animate-spin fill-contrast"
+                              viewBox="0 0 100 101"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                              />
+                              <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                              />
+                            </svg>
+                            <span className="sr-only">Loading...</span>
+                          </div>
+                        </div>
+                      )}
                     </div>
 
                     <div className="flex items-center align-end text-sm justify-end">
