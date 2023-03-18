e4747339d3e56c4d418c967d2813af3a7b9b8e68
carvtmux-sessionizeralho28 <carvalhoo287@gmail.com>
2023-03-18 12:08:59 +0000

    Change name on profile

---
+++ components/ModelForm.tsx
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import { useState } from "react";
+
+const ModelForm = () => {
+  const [name, setName] = useState<string | undefined>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  return (
+    <div
+      id="authentication-modal"
+      tabIndex={-1}
+      aria-hidden="true"
+      className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full"
+    >
+      <div className="relative w-full h-full max-w-md md:h-auto">
+        <div className="mt-6">
+          <form
+            className="space-y-6"
+            //   onSubmit={signUpEmail}
+          >
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="name"
+                  autoComplete="name"
+                  placeholder="Your name"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-contrast sm:text-sm sm:leading-6 pl-4"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="space-y-1">
+              {loading && (
+                <div className="flex justify-center">
+                  <div role="status">
+                    <svg
+                      aria-hidden="true"
+                      className="inline w-10 h-10 mr-2 mt-5 text-gray-200 animate-spin fill-contrast"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div className="flex items-center align-end text-sm justify-end">
+              <Link
+                href="/login"
+                className="font-medium text-contrast hover:text-contrasthover"
+              >
+                Already have an account?
+              </Link>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-contrast py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-contrasthover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-contrast"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModelForm;
--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FaPen } from "react-icons/fa";
+import Link from "next/link";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const supabase = createServerSupabaseClient(ctx);
@@ -44,20 +45,43 @@ export default function Profile({ avatar_url }: any) {
   const user = useUser();
 
   const [userData, setUserData] = useState<any>(null);
+  const [name, setName] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState(false);
+
+  function toggleModal() {
+    console.log("toggle");
+    setShowModal(!showModal);
+  }
+
+  async function saveName() {
+    const { error } = await supabaseClient
+      .from("profiles")
+      .update({ name: name })
+      .eq("id", user?.id);
+    if (!error) {
+      setUserData({ ...userData, name: name });
+    }
+
+    toggleModal();
+  }
 
   useEffect(() => {
     // get user data
     async function getUserData() {
-      const { data, error } = await supabaseClient
-        .from("profiles")
-        .select("*")
-        .eq("id", user?.id);
-      if (error) console.log(error);
-      if (!data) {
-        console.log("No data found");
-        return;
+      if (user) {
+        const { data, error } = await supabaseClient
+          .from("profiles")
+          .select("*")
+          .eq("id", user?.id);
+        if (error) console.log(error);
+        if (!data) {
+          console.log("No data found");
+          return;
+        }
+        setUserData(data[0]);
+        setName(data[0].name);
       }
-      setUserData(data[0]);
     }
 
     getUserData();
@@ -65,11 +89,10 @@ export default function Profile({ avatar_url }: any) {
 
   return (
     <Layout currentPage="profile" avatarUrl={avatar_url}>
-      <div className="flex max-w-lg p-6 bg-white rounded-lg shadow-lg">
-        <div className="w-1/2 flex flex-col justify-center items-center">
-          {/* load avatar */}
+      <div className="flex p-6 bg-white rounded-lg shadow-lg w-3/5 h-auto">
+        <div className="w-2/5 flex flex-col justify-center items-center">
           <Image
-            className="object-cover w-full h-54 rounded-lg"
+            className="object-cover w-full rounded-lg"
             src={avatar_url}
             alt="avatar"
             width={300}
@@ -84,16 +107,81 @@ export default function Profile({ avatar_url }: any) {
         </div>
 
         <div className="w-px bg-gray-400 mx-4"></div>
-        <div className="w-1/2 p-6">
+
+        <div className="w-3/5 p-6">
           <div className="flex items-center">
             <h3 className="text-black font-semibold text-lg">
               {userData?.name}
             </h3>
-            <button className="flex items-center ml-4 mb-2">
+            <button
+              className="flex items-center ml-4 mb-2"
+              onClick={() => toggleModal()}
+            >
               <FaPen />
             </button>
           </div>
         </div>
+      </div>
+      <div>
+        {showModal && (
+          // modal to change name
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <div
+                className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                aria-hidden="true"
+              ></div>
+
+              <span
+                className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                aria-hidden="true"
+              >
+                &#8203;
+              </span>
+
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 flex justify-center items-center">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4">
+                      <h3
+                        className="text-lg leading-6 font-medium text-black"
+                        id="modal-headline"
+                      >
+                        Change your name
+                      </h3>
+                      <div className="mt-6 w-72">
+                        <input
+                          type="text"
+                          name="name"
+                          id="name"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                          className="shadow-sm focus:ring-contrast focus:border-contrast block w-full sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-white px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-contrast text-base font-medium text-white hover:bg-contrasthover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-contrast sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => saveName()}
+                  >
+                    Save
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-contrast sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => toggleModal()}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </Layout>
   );
