801f94d8a9d5573bc411c9a56f846b3f7071ecb4
carvtmux-sessionizeralho28 <carvalhoo287@gmail.com>
2023-03-19 16:45:08 +0000

    Added base form for new project

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -211,7 +211,7 @@ export default function Profile({ avatar_url }: any) {
                 <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                   <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 flex justify-center items-center">
                     <div className="sm:flex sm:items-start">
-                      <div className="mt-3 text-center sm:mt-0 sm:ml-4">
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 ml-2">
                         <h3
                           className="text-lg leading-6 font-medium text-black"
                           id="modal-headline"
@@ -231,7 +231,7 @@ export default function Profile({ avatar_url }: any) {
                       </div>
                     </div>
                   </div>
-                  <div className="bg-white px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <div className="bg-white px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse justify-center">
                     <button
                       type="button"
                       className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-contrast text-base font-medium text-white hover:bg-contrasthover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-contrast sm:ml-3 sm:w-auto sm:text-sm"
--- pages/projects/table.tsx
+++ pages/projects/table.tsx
@@ -4,6 +4,7 @@ import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const Tabs = dynamic(() => import("@/components/Tabs"), { ssr: false });
 
@@ -79,6 +80,11 @@ export default function Projects({ avatar_url }: any) {
   ];
 
   const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+
+  function toggleModal() {
+    setShowModal(!showModal);
+  }
 
   return (
     <div>
@@ -96,6 +102,9 @@ export default function Projects({ avatar_url }: any) {
                 <button
                   type="button"
                   className="block rounded-md bg-contrast py-2 px-3 text-center text-sm font-semibold text-white shadow-sm hover:bg-contrasthover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-contrast"
+                  onClick={() => {
+                    toggleModal();
+                  }}
                 >
                   Add project
                 </button>
@@ -201,6 +210,79 @@ export default function Projects({ avatar_url }: any) {
               </table>
             </div>
           </div>
+        </div>
+        <div>
+          {showModal && (
+            <div className="fixed z-10 inset-0 overflow-y-auto">
+              <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div
+                  className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                  aria-hidden="true"
+                ></div>
+
+                <span
+                  className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+
+                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 flex justify-center items-center">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 ml-2">
+                        <h3
+                          className="text-lg leading-6 font-medium text-black"
+                          id="modal-headline"
+                        >
+                          New project
+                        </h3>
+                        <h5>
+                          <span className="text-gray-500">Name</span>
+                        </h5>
+                        <div className="mt-6 w-72">
+                          <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            // value={name}
+                            // onChange={(e) => setName(e.target.value)}
+                            className="shadow-sm focus:ring-contrast focus:border-contrast block w-full sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div>
+                        {/* <div className="mt-6 w-72">
+                          <input
+                            type="textarea"
+                            name="name"
+                            id="name"
+                            // value={name}
+                            // onChange={(e) => setName(e.target.value)}
+                            className="shadow-sm focus:ring-contrast focus:border-contrast block w-full sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div> */}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-white px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse justify-center">
+                    <button
+                      type="button"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-contrast text-base font-medium text-white hover:bg-contrasthover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-contrast sm:ml-3 sm:w-auto sm:text-sm"
+                      // onClick={() => saveName()}
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-contrast sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={() => toggleModal()}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </Layout>
     </div>
