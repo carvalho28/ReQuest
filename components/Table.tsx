ae8a2be8df81e2ea7a863f73dc62543ec769724e
carvalho28 <carvalhoo287@gmail.com>
2023-04-23 22:12:19 +0100

    feat: added page for chat

--- components/Table.tsx
+++ components/Table.tsx
@@ -11,7 +11,6 @@ import {
 import { useRowSelectColumn } from "@lineup-lite/hooks";
 import {
   RiAddLine,
-  RiAliensLine,
   RiArrowDownSLine,
   RiArrowLeftCircleFill,
   RiArrowRightCircleFill,
---
+++ pages/chat.tsx
@@ -0,0 +1,70 @@
+import Layout from "@/components/Layout";
+import { ProjectChildren } from "@/components/utils/sidebarHelper";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { GetServerSidePropsContext } from "next";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const supabase = createServerSupabaseClient(ctx);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (!session)
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+
+  const user = session.user;
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("avatar_url")
+    .eq("id", user?.id);
+  if (error) console.log(error);
+  if (!data) throw new Error("No data found");
+  const avatar_url = data[0].avatar_url;
+
+  // get user projects info
+  const { data: dataProjects, error: errorProjects } = await supabase.rpc(
+    "projects_user",
+    { user_id: user?.id }
+  );
+
+  // convert to a ProjectChildren type where href is the /projects/[id] route
+  const projectsChildren: ProjectChildren[] = dataProjects.map(
+    (project: any) => {
+      return {
+        name: project.name,
+        href: `/projects/${project.id}`,
+      };
+    }
+  );
+
+  return {
+    props: {
+      avatar_url: avatar_url,
+      projectsChildren: projectsChildren,
+    },
+  };
+};
+
+
+export default function Chat({ avatar_url, projectsChildren }: any) {
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
+
+  return (
+    <div>
+      <Layout
+        currentPage="Chat"
+        avatar_url={avatar_url}
+        projectChildren={projectsChildren}
+      >
+        Algo
+      </Layout>
+    </div>
+  );
+}
