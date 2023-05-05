f3de4ac6689cbbc429b3d48101801bd2beda71fc
carvalho28 <carvalhoo287@gmail.com>
2023-05-05 21:53:29 +0100

    feat: changed ranking aspect

--- components/ListRanking.tsx
+++ components/ListRanking.tsx
@@ -1,5 +1,7 @@
 import { Rankings } from "@/pages/projects/[id]";
 import { Rubik_Bubbles } from "next/font/google";
+import Image from "next/image";
+import { renderImage } from "./utils/general";
 
 const rubikBubbles = Rubik_Bubbles({
   subsets: ["latin"],
@@ -12,58 +14,73 @@ interface ListRankingProps {
 
 const ListRanking = ({ rankings }: ListRankingProps) => {
   const rankings3 = [...rankings, ...rankings, ...rankings];
+
+  const cups = [
+    "/gold-cup.svg",
+    "/silver-cup.svg",
+    "/bronze-cup.svg",
+    "/purple-cup.svg"
+  ];
+
+  function generateCup (index: number) {
+    if ( index == 0) {
+      return cups[0];
+    } else if (index == 1) {
+      return cups[1];
+    } else if (index == 2) {
+      return cups[2];
+    } else {
+      return cups[3];
+    }
+  }
+
   return (
     <div className="overflow-y-scroll w-full h-60 scroll">
-      <dl className="grid grid-cols-1 divide-y divide-gray-400 rounded-lg bg-white border border-gray-400 h-54">
+      <dl className="grid grid-cols-1 rounded-lg h-54 space-y-2">
         {rankings3.map((item, index) => (
-          <div key={item.id} className=" flex flex-row p-2">
-            {/* <dt>{index}</dt> */}
-            <dt className="flex flex-col items-center justify-center border-r border-gray-200 pr-2">
-              <div
-                className={`flex flex-row items-center justify-center rounded-full w-8 h-8 
-                ${index === 0 && "bg-yellow-400"}
-                ${index === 1 && "bg-gray-400"}
-                ${index === 2 && "bg-yellow-600"}
-                ${index > 2 && "bg-purple-300"}
-                `}
-              >
-                {index + 1}
-                {/* if 1 write st, 2 write sd ... */}
-                {index === 0 && <div className="text-xs">st</div>}
-                {index === 1 && <div className="text-xs">nd</div>}
-                {index === 2 && <div className="text-xs">rd</div>}
-                {index > 2 && <div className="text-xs">th</div>}
+          <div key={item.id} className=" flex flex-row p-2 bg-gray-50">
+            <dt
+              className="flex flex-col items-center justify-center
+              pr-2"
+            >
+              <div className="px-3">
+                <Image
+                  className="h-12 w-12 rounded-full"
+                  alt="avatar"
+                  src={"data:image/svg+xml," + renderImage(item.avatar_url)}
+                  width={40}
+                  height={40}
+                />
               </div>
             </dt>
             {/* image */}
-            <div className="flex flex-col items-start ml-4">
-              {/* <div className="flex-shrink-0">
-                <img className="h-10 w-10 rounded-full" alt="avatar" />
-              </div> */}
+            <div className="flex flex-col items-start ml-2">
               <div className="flex flex-row items-center">
-                {/* <Image
-                  className="h-12 w-12 rounded-full"
-                  alt="avatar"
-                  src={item.avatar_url}
-                  width={40}
-                  height={40}
-                /> */}
-                {/* <div className="flex flex-col items-start">{index}</div> */}
                 <div className="ml-2 text-md font-medium text-gray-900">
                   {item.name}
                 </div>
               </div>
               <div className={`flex flex-row items-center`}>
-                <div className="ml-2 text-xs text-gray-500 truncate">
-                  Requirements closed:
+                <div className="ml-1 text-xs text-gray-500 truncate">
+                  Req. closed:
                 </div>
 
                 <div
-                  className={`ml-2 text-md font-medium text-gray-900 ${rubikBubbles.className}`}
+                  className={`ml-2 text-sm font-medium text-gray-900 items-center`}
                 >
                   {item.requirements_closed}
                 </div>
               </div>
+            </div>
+            <div className="ml-auto px-3">
+              <Image
+                  className="h-12 w-12 rounded-full"
+                  alt="avatar"
+                  src={generateCup(index)}
+                  width={40}
+                  height={40}
+                />
+              
             </div>
           </div>
         ))}
--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -10,7 +10,6 @@ import Image from "next/image";
 import { Rubik_Glitch } from "next/font/google";
 import CountdownTimer from "@/components/CountdownTimer";
 import { UserIdAndName } from "@/components/utils/general";
-import Leaderboard from "@/components/Leaderboard";
 import ListRanking from "@/components/ListRanking";
 import dynamic from "next/dynamic";
 
@@ -248,7 +247,7 @@ export default function SingleProject({
         projectChildren={projectsChildren}
       >
         <div className="flex gap-x-4 mt-8 flex-col md:flex-row gap-y-8">
-          <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg justify-center md:w-1/4 w-full">
+          <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg justify-center md:w-1/3 w-full">
             <h3 className="text-xl font-bold flex justify-center items-center text-center">
               Progress
             </h3>
@@ -266,8 +265,9 @@ export default function SingleProject({
               <div
                 className="h-4 rounded-full"
                 style={{
-                  width: `${(requirementsCompleted / totalRequirements) * 100
-                    }%`,
+                  width: `${
+                    (requirementsCompleted / totalRequirements) * 100
+                  }%`,
                   backgroundColor: getColor(
                     (requirementsCompleted / totalRequirements) * 100
                   ),
@@ -283,7 +283,7 @@ export default function SingleProject({
               </div>
             </div>
           </div>
-          <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg justify-center md:w-1/4 w-full">
+          <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg justify-center md:w-1/3 w-full">
             <h3 className="text-xl font-bold flex justify-center items-center text-center">
               Deadline
             </h3>
@@ -305,19 +305,10 @@ export default function SingleProject({
             </div>
           </div>
 
-          <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg md:w-2/4 w-full">
+          <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg md:w-1/3 w-full">
             <h3 className="text-xl font-bold flex justify-center">Ranking</h3>
             <div className="flex flex-col justify-center mt-8 space-y-4 items-center">
-              <div className="flex flex-row w-full lg2:space-x-8 justify-center">
-                <div className="w-2/5 hidden lg2:flex">
-                  {ranking.length > 0 && <Leaderboard rankings={ranking} />}
-                </div>
-                {/* divider line */}
-                <div className="border-r-2 border-gray-300 hidden lg2:flex"></div>
-                <div className="lg2:w-3/5 w-full">
-                  <ListRanking rankings={ranking} />
-                </div>
-              </div>
+              <ListRanking rankings={ranking} />
             </div>
           </div>
         </div>
