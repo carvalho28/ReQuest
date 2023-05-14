ab966b1d5c128d32e071f79e4dc7e3e657dd8e93
carvalho28 <carvalhoo287@gmail.com>
2023-05-14 15:26:32 +0100

    fix: fixed onclick target

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
+const {DOCS_URL} = process.env;
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
     domains: [process.env.NEXT_PUBLIC_IMAGE_DOMAINS],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/docs',
+        destination: `${DOCS_URL}/docs`,
+      },
+      {
+        source: '/docs/:path*',
+        destination: `${DOCS_URL}/docs/:path*`,
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;
