eb3f9f9efc6337c4290e6b7b1254ad26bb7c995f
carvalho28 <carvalhoo287@gmail.com>
2023-06-28 12:11:32 +0100

    docs: updated gemified

--- request-docs/next.config.js
+++ request-docs/next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
+  latex: true,
 });
 
 module.exports = withNextra(nextConfig);
